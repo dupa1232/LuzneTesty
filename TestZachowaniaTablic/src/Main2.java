4afb2a3ece9768ac197bc202d0040d9c1c9c21bb
dupa1232 <misiu889@interia.pl>
2022-01-31 20:07:12 +0100

    Test clone() na tablicy part 2

--- TestZachowaniaTablic/src/Main2.java
+++ TestZachowaniaTablic/src/Main2.java
@@ -14,6 +14,19 @@ public class Main2 {
         }
         System.out.println(tablica.length); // 4
         System.out.println(tablica == tablica2); // false
-        System.out.println(tablica.equals(tablica2)); // false
+        System.out.println(tablica.equals(tablica2)); // false\
+        System.out.println(tablica[1] == tablica2[1]); // true
+//        System.out.println(tablica[1].equals(tablica2[1])); /// takie co¶ nie jest mo¿liwe, bo mamy tablicê prymitywów
+
+        //// DU¯E INTEGER-YYYyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+        System.out.println("INTEGEEEERyyyy");
+        Integer[] tablicaInteger = {3,6,2,2};
+        Integer[] tablicaInteger2 = tablicaInteger.clone();
+
+        System.out.println(tablicaInteger.length); // 4
+        System.out.println(tablicaInteger == tablicaInteger2); // false
+        System.out.println(tablicaInteger.equals(tablicaInteger2)); // false
+        System.out.println(tablicaInteger[1] == tablicaInteger2[1]); //true
+        System.out.println(tablicaInteger[1].equals(tablicaInteger2[1])); //true
     }
 }
